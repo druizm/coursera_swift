08823ec3b84ab1dc32b2da0e47bafc24e49c7c91
David M <davidm@Davids-Mac.local>
2016-03-20 04:21:28 -0500

    Tarea Sem 4

---
+++ Sem4_Tarea.playground/Contents.swift
@@ -0,0 +1,67 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+    
+}
+
+class Auto {
+    var velocidad : Velocidades
+
+    init (){
+        self.velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
+    }
+
+    func cambioDeVelocidad () -> (actual : Int, velocidadEnCadena : String) {
+        
+        var velocidadActualEnNumero = 0
+        var velocidadActualEnCadena = ""
+        
+        switch velocidad.rawValue {
+        case 0 :
+            velocidadActualEnNumero = 0
+            velocidadActualEnCadena = "Apagado"
+            self.velocidad = Velocidades.VelocidadBaja
+            break
+        case 20 :
+            velocidadActualEnNumero = 20
+            velocidadActualEnCadena = "Velocidad baja"
+            self.velocidad = Velocidades.VelocidadMedia
+            break
+        case 50 :
+            velocidadActualEnNumero = 50
+            velocidadActualEnCadena = "Velocidad media"
+            self.velocidad = Velocidades.VelocidadAlta
+            break
+        case 120 :
+            velocidadActualEnNumero = 120
+            velocidadActualEnCadena = "Velocidad alta"
+            self.velocidad = Velocidades.VelocidadMedia
+            break
+        default:
+            break
+            
+        }
+        
+        let resultado = ( velocidadActualEnNumero, velocidadActualEnCadena )
+        return resultado
+    }
+    
+}
+
+
+var auto = Auto()
+
+for index in 0...19 {
+    
+    let resultado = auto.cambioDeVelocidad()
+    
+    print("\(resultado.actual), \(resultado.velocidadEnCadena)")
+    
+}
